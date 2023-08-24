94ffc7396638b176c073f63c5f8f277190b8d837
Mark <a1234@ZolotaySeredina.local>
2023-08-24 09:32:24 +0300

    DZ

---
+++ task001/Program.cs
@@ -0,0 +1,17 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из диапозона 100-999 -> {number} ");
+
+int firstDigit = number / 100;
+int lastDigit = number % 10;
+int result = firstDigit * 10 + lastDigit;
+Console.WriteLine(result);
+
+int TwoNumbers(int num)
+{
+    int firstDigit = number / 100;
+    int lastDigit = number % 10;
+    int result = firstDigit * 10 + lastDigit;
+    return result;
+}
+int twoNumbers = TwoNumbers(number);
+Console.WriteLine(TwoNumbers);
