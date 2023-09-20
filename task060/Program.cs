8c5e3cd1e82dac81b315dfe99842f05c663e0cc1
Mark <you@example.com>
2023-09-20 15:52:41 +0300

    DZ

--- task060/Program.cs
+++ task060/Program.cs
@@ -6,7 +6,7 @@
 int[,,] CreateMatrixRndInt(int rows, int columns, int array3, int min, int max)
 {
     int[,,] matrix = new int[rows, columns, array3];
-    Random rnd = new Random((DateTime.Now.Ticks));
+    Random rnd = new Random();
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
---
+++ task066/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+ */
+
+Console.WriteLine("Введите натуральное число M: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число N: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (number > number1)
+    {
+        Console.WriteLine("Значение M должно быть меньше или равно N");
+        return;
+    }
+
+int SumNumbers(int num, int num1)
+{
+    int sum = num;
+
+    if (num == num1) return num;
+    else
+    {
+        sum += SumNumbers(num + 1, num1);
+    }
+    return sum;
+}
+
+int sumNumbers = SumNumbers(number, number1);
+Console.WriteLine(sumNumbers);
